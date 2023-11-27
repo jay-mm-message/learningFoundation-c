bd3813e9bb3b3b9b4439ee467ada77d5653a14ad
jay <jay.mm.message@gmail.com>
2023-11-27 19:44:29 +0800

    Create queue using linked list

--- linkedlists/main.c
+++ linkedlists/main.c
@@ -7,6 +7,26 @@ typedef struct Node {
     struct Node * next;
 } Node;
 
+// Create queue using linked list.
+void push_qt(struct Node **head_ref, int data) {
+ 
+    struct Node * newNode = (struct Node *)
+        malloc(sizeof(struct Node));
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (NULL == (*head_ref)) {
+        (*head_ref) = newNode;
+    } else {
+        struct Node * tail = (*head_ref);
+        while(NULL != tail->next) {
+            tail = tail->next;
+        }
+        tail->next = newNode;
+    }
+}
+
 // Create stack using linked list.
 void push_st(struct Node **head_ref, int data) {
     struct Node * newNode = (struct Node *)
@@ -26,12 +46,21 @@ void show(struct Node * head_ref) {
 
 int main() {
 
+    printf("Create stack using linked list.\n");
     Node *head = NULL;
     for(int i = 1 ; i < 6 ; ++i) {
         push_st(&head, 100 + i);
     }
 
     show(head);
+    
+    printf("Create queue using linked list.\n");
+    Node *head_2 = NULL;
+    for(int i = 1 ; i < 6 ; ++i) {
+        push_qt(&head_2, 100 + i);
+    }
+
+    show(head_2);
 
     printf("\n");
     return 0;
